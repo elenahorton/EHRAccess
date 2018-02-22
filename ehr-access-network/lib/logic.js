c21b0b599ae5ffbd3b0fa129f3ed6651b01c9ac8
Elena Horton <elena.n.horton.18@dartmouth.edu>
2018-02-21 19:50:10 -0500

    added programmatic access control

--- ehr-access-network/lib/logic.js
+++ ehr-access-network/lib/logic.js
@@ -1,31 +1,6 @@
 'use strict';
 /**
- * Write your transction processor functions here
- */
-
-/**
- * Sample transaction
- * @param {org.acme.biznet.Access} access
- * @transaction
- */
-// function onChangeAssetValue(changeAssetValue) {
-//     var assetRegistry;
-//     var id = changeAssetValue.relatedAsset.assetId;
-//     return getAssetRegistry('org.acme.biznet.SampleAsset')
-//         .then(function(ar) {
-//             assetRegistry = ar;
-//             return assetRegistry.get(id);
-//         })
-//         .then(function(asset) {
-//             asset.value = changeAssetValue.newValue;
-//             return assetRegistry.update(asset);
-//         });
-// }
-
-/**
- * Grant or deny access to a specific file by a requesting entity
- * @param {org.acme.biznet.AccessRequest} access request - the access request to be tested, then granted or denied
- * @transaction
+ * Transaction processor functions
  */
 
 /** A transaction processor function description
@@ -34,9 +9,19 @@
 */
 
 function handleAccessRequest(accessRequest) {
+
+    var me = getCurrentParticipant();
+    console.log('@debug: **** REQUESTING ACCESS: ' + me.getIdentifier() + ' requesting access of ' + accessRequest.fileAccess);
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+    if (me.getIdentifier() != accessRequest.requestingProvider.entityID) {
+        throw new Error('Requesting provider is not the current participant, request denied.')
+    }
  
-    var roleOrgIDs = accessRequest.requestingProvider.roleOrgs;
   	var len=accessRequest.fileAccess.accessList.length;
+    var roleOrgIDs = accessRequest.requestingProvider.roleOrgs;
   	var lenRoles = accessRequest.requestingProvider.roleOrgs.length;
     var success = 0;
     var blocked = 0;
@@ -81,6 +66,13 @@ function handleAccessRequest(accessRequest) {
 */
 
 function handleAccessGrant(accessGrant) {
+
+    var fileOwner = accessGrant.fileAccess.fileOwner;
+    var currentParticipant = getCurrentParticipant();
+    if (currentParticipant.getFullyQualifiedIdentifier() !== fileOwner.getFullyQualifiedIdentifier()) {
+        throw new Error('Access can only be granted to this file by the owner of this file');
+        return;
+    }
   
     accessGrant.fileAccess.accessList.push(accessGrant.accessEntity);
     
@@ -91,12 +83,18 @@ function handleAccessGrant(accessGrant) {
 }
 
 /** A transaction processor function description
-* @param {org.acme.biznet.AccessBlock} accessBlock- the access to be blocked
+* @param {org.acme.biznet.AccessBlock} accessBlock- the access asset to be blocked
 * @transaction
 */
 
 function handleAccessBlock(accessBlock) {
   
+    var fileOwner = accessBlock.fileAccess.fileOwner;
+    var currentParticipant = getCurrentParticipant();
+    if (currentParticipant.getFullyQualifiedIdentifier() !== fileOwner.getFullyQualifiedIdentifier()) {
+        throw new Error('Access can only be blocked to this file by the owner of this file');
+        return;
+    }
     accessBlock.fileAccess.blockedList.push(accessBlock.blockedEntity);
     
     return getAssetRegistry('org.acme.biznet.Access')
@@ -106,16 +104,29 @@ function handleAccessBlock(accessBlock) {
 }
 
 /** A transaction processor function description
+ * Only provider admins can add or revoke roles to and from providers
 * @param {org.acme.biznet.AddRoleOrg} addingRoleOrg-the role org transaction to be added to a provider
 * @transaction
 */
 
 function handleAddingRoleOrg(addingRoleOrg) {
+
+    var me = getCurrentParticipant();
+    console.log('@debug: **** ADDING ROLEORG: ' + me.getIdentifier() + ' adding access of ' + addingRoleOrg.provider + ' to ' + addingRoleOrg.roleOrg);
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+    if (me.getFullyQualifiedType() != "org.acme.biznet.ProviderAdmin") {
+        console.log('@debug: requesting provider: ' + me.getIdentifier() + ' is not a provider admin, denying transaction');
+        throw new Error('Current participant is not an admin, cannot add or revoke roles to/from providers.');
+        return
+    }
   
     addingRoleOrg.provider.roleOrgs.push(addingRoleOrg.roleOrg);
     addingRoleOrg.roleOrg.providers.push(addingRoleOrg.provider);
     
-    var updateList1 = getParticipantRegistry('org.acme.biznet.Provider')
+    var updateList1 = getParticipantRegistry('org.acme.biznet.ProviderAdmin')
         .then(function (participantRegistry) {
       return participantRegistry.update(addingRoleOrg.provider);
     });
@@ -129,12 +140,25 @@ function handleAddingRoleOrg(addingRoleOrg) {
 }
 
 /** A transaction processor function description
+ * Only provider admins can add or revoke roles to and from providers
 * @param {org.acme.biznet.RevokeRoleOrg} revokingRoleOrg-the role org transaction to be revoked from a provider
 * @transaction
 */
 
 function handleRevokingRoleOrg(revokingRoleOrg) {
   
+    var me = getCurrentParticipant();
+    console.log('@debug: **** REVOKING ROLEORG: ' + me.getIdentifier() + ' revoking access of ' + revokingRoleOrg.provider + ' from ' + revokingRoleOrg.roleOrg);
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+    if (me.getFullyQualifiedType() != "org.acme.biznet.ProviderAdmin") {
+        console.log('@debug: requesting provider: ' + me.getIdentifier() + ' is not a provider admin, denying transaction');
+        throw new Error('Current participant is not an admin, cannot add or revoke roles to/from providers.');
+        return
+    }
+
     var index = revokingRoleOrg.provider.roleOrgs.indexOf(revokingRoleOrg.roleOrg);
     if (index !== -1) {
         revokingRoleOrg.provider.roleOrgs.splice(index, 1);
